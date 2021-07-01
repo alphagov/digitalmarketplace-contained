2b2ab5b1eabb7205444e4bd8c1ddb2c5bfda8cbb
Daniele Occhipinti <22837571+daniele-occhipinti@users.noreply.github.com>
2021-07-01 15:52:34 +0100

    Fix typo

--- files-for-container/setup_runner.py
+++ files-for-container/setup_runner.py
@@ -65,7 +65,7 @@ class SetupRunner:
                 # In python 3.6+, it seems that dict loading order is preserved (source:
                 # https://stackoverflow.com/questions/39980323/are-dictionaries-ordered-in-python-3-6) Therefore,
                 # to keep things simple, we can ignore the 'run-order' attribute in the settings.yml file and imply
-                # the order the apps are listed in the file is the right order for execution
+                # the order the apps as listed in the file is the right order for execution
                 settings: dict = yaml.safe_load(stream)
 
                 repository_name: str
